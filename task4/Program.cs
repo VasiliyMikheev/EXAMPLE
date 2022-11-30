8094c4577328c0cf683a836e6cdc71692617ec43
VasiliyMikheev <118388942+VasiliyMikheev@users.noreply.github.com>
2022-12-01 00:50:55 +0300

    Упражнеие 5 с именами приветствие

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "василий")
+{
+    Console.Write("Хелоу Васяня");
+}
+else
+{
+ Console.Write ("Привет, ");
+ Console.WriteLine(username);  
+}

7d0f68e68e5cd45479a44c28a1455fd28c3fbe50
VasiliyMikheev <118388942+VasiliyMikheev@users.noreply.github.com>
2022-12-05 20:10:04 +0300

    обновление после прохождения 2го семинара

---
+++ task6MAGIC/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+
+int  xa = 20, ya = 1,
+     xb = 11, yb = 30,
+     xc = 40, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine ("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine ("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine ("+");
+
+int x = xa, y = xb;
+
+int count = 0
+
+while (count < 10)
+{
+    int what = new Random().Next (0, 3)
+ if (what == 0)
+ {
+    x = (x + xa) / 2;
+ }   
+}
